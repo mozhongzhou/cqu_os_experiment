d5a04e2001ee399ee4028c6ddc62ef6adc2aba48
漠中洲 <2014160588@qq.com>
2024-05-13 14:44:16 +0800

    1

--- epos/kernel/sem.c
+++ epos/kernel/sem.c
@@ -7,9 +7,14 @@
 struct sem *sem_cur;
 // 信号量空间头部
 struct sem *sem_start;
-
 int num = 0;
 
+/**
+ * sys_sem_create(int value): 创建一个新的信号量，并将其初始值设置为value。
+ * 如果是第一个创建的信号量，那么就分配一块新的内存给sem_cur，并将sem_start设置为sem_cur。
+ * 否则，将sem_cur设置为sem_start后面的一个位置。
+ * 最后，将信号量的值设置为value，并将等待队列设置为NULL。
+ */
 int sys_sem_create(int value)
 {
     if (!num)
@@ -26,7 +31,10 @@ int sys_sem_create(int value)
     num++;
     return (num - 1);
 }
-
+/**
+ * sys_sem_destroy(int semid): 销毁一个已经存在的信号量。
+ * 如果semid在有效范围内（即0到num之间），那么就释放对应的内存，并将信号量的数量减一。
+ */
 int sys_sem_destroy(int semid)
 { // 0<=semid<num
     if (semid < num && semid >= 0)
@@ -41,7 +49,11 @@ int sys_sem_destroy(int semid)
         return -1;
     }
 }
-
+/**
+ * sys_sem_wait(int semid): 对一个已经存在的信号量进行等待操作。
+ * 如果semid在有效范围内，那么就将对应的信号量的值减一。
+ * 如果信号量的值小于0，那么就将当前进程放入等待队列。
+ */
 int sys_sem_wait(int semid)
 { // 0<=semid<num
     if (semid < num && semid >= 0)
@@ -64,7 +76,11 @@ int sys_sem_wait(int semid)
         return -1;
     }
 }
-
+/**
+ * sys_sem_signal(int semid): 对一个已经存在的信号量进行信号操作。
+ * 如果semid在有效范围内，那么就将对应的信号量的值加一。
+ * 如果信号量的值小于或等于0，那么就唤醒等待队列中的一个进程。
+ */
 int sys_sem_signal(int semid)
 { // 0<=semid<num
     if (semid < num && semid >= 0)
--- epos/userapp/main.c
+++ epos/userapp/main.c
@@ -20,15 +20,27 @@ extern void *g_heap;
 // 间隔值：用于描绘nice实体
 #define emp2 2
 #define N 200
+/**
+在生产者-消费者模型中，使用信号量的典型实现如下：
+使用一个信号量 empty 来表示缓冲区中空闲的位置数量。
+使用一个信号量 full 来表示缓冲区中已填入数据的位置数量。
+生产者在向缓冲区中插入数据之前，必须获取 empty 信号量，以确保缓冲区中有空闲位置可用。
+消费者在从缓冲区中取出数据之前，必须获取 full 信号量，以确保缓冲区中有数据可用。
+生产者向缓冲区中插入数据后，会释放 full 信号量，表示有数据可用。
+消费者从缓冲区中取出数据后，会释放 empty 信号量，表示有空闲位置可用。
+*/
+void sort_bubble(int ary[], int curConsumer);
+void tsk_control(void *pv);
+void tsk_producer(void *pv);
+void tsk_consumer(void *pv);
+static int mutex = 1;  // 互斥信号量 （初始值为1 表示一次只能有1个线程访问缓冲区
+static int full = 0;   // 消费者在从缓冲区中取出数据之前，必须获取 full 信号量，以确保缓冲区中有数据可用
+static int empty = 10; // 生产者在向缓冲区中插入数据之前，必须获取 empty 信号量，以确保缓冲区中有空闲位置可用
+int curProducer = 0;   // 当前生产者到哪了
+int curConsumer = 0;   // 当前消费者到哪了
+int ary[N];
 
-void tsk_1(void *pv);
-void tsk_2(void *pv);
-void tsk_3(void *pv);
-void sort_bubble1(int ary[]);
-void sort_bubble2(int ary[]);
-
-int ary1[N], ary2[N];
-int tid1, tid2;
+int tid_producer, tid_consumer, tid_control;
 /**
  * GCC insists on __main
  *    http://gcc.gnu.org/onlinedocs/gccint/Collect2.html
@@ -46,76 +58,46 @@ void __main()
 
 void main(void *pv)
 {
-    srand(time(NULL));
-    int i, temp;
-    for (i = 0; i < N; i++)
-    {
-        temp = rand() % 400;
-        ary1[i] = temp;
-        ary2[i] = temp;
-    }
 
     int mode = 0x143;
     init_graphic(mode);
-    line(400, 0, 400, 600, RGB(255, 0, 0)); // 屏幕中间一根红线分隔开
-    // Apply for the stack
+    srand(time(NULL));
+    int i;
+    int width = g_graphic_dev.XResolution / 10;
+    for (i = 0; i < N; i++)
+    {
+        ary[i] = rand() % width; // 控制横着的宽度 不会超过边界
+    }
+
+    // 开辟空间
     unsigned int stack_size = 1024 * 1024;
-    unsigned char *stack_foo1 = (unsigned char *)malloc(stack_size);
-    unsigned char *stack_foo2 = (unsigned char *)malloc(stack_size);
-    unsigned char *stack_foo3 = (unsigned char *)malloc(stack_size);
+    unsigned char *stack_producer = (unsigned char *)malloc(stack_size);
+    unsigned char *stack_consumer = (unsigned char *)malloc(stack_size);
+    unsigned char *stack_control = (unsigned char *)malloc(stack_size);
 
-    // Creat the thread
-    int tid_1, tid_2, tid_3;
+    // 创建线程
+    int tid_producer, tid_consumer, tid_control;
 
-    tid_1 = task_create(stack_foo1 + stack_size, &tsk_1, (void *)0);
-    setpriority(tid_1, 15);
-    tid_2 = task_create(stack_foo2 + stack_size, &tsk_2, (void *)0);
-    setpriority(tid_2, 15);
-    tid_3 = task_create(stack_foo3 + stack_size, &tsk_3, (void *)0);
-    setpriority(tid_3, 0);
+    tid_producer = task_create(stack_producer + stack_size, &tsk_producer, (void *)0);
+    setpriority(tid_producer, 15);
+    tid_consumer = task_create(stack_consumer + stack_size, &tsk_consumer, (void *)0);
+    setpriority(tid_consumer, 15);
+    tid_control = task_create(stack_control + stack_size, &tsk_control, (void *)0);
+    setpriority(tid_control, 0);
 
-    tid1 = tid_1;
-    tid2 = tid_2;
-    // wait for the thread to exit
-    task_wait(tid_1, NULL);
-    task_wait(tid_2, NULL);
-    task_wait(tid_3, NULL);
+    task_wait(tid_producer, NULL);
+    task_wait(tid_consumer, NULL);
+    task_wait(tid_control, NULL);
 
-    // Free the stack
-    free(stack_foo1);
-    free(stack_foo2);
-    free(stack_foo3);
+    sem_destroy(tid_producer);
+    sem_destroy(tid_consumer);
+    sem_destroy(tid_control);
     while (1)
         ;
     task_exit(0);
 }
 
-void tsk_1(void *pv)
-{
-
-    int gap = 2;
-    int i;
-    for (i = 0; i < N; i++)
-    {
-        line(0, i * gap, ary1[i], i * gap, RGB(128, 0, 128));
-    }
-    sort_bubble1(ary1);
-    task_exit(0);
-}
-void tsk_2(void *pv)
-{
-
-    int gap = 2;
-    int i;
-    for (i = 0; i < N; i++)
-    {
-        line(400, i * gap, 400 + ary2[i], i * gap, RGB(255, 165, 0));
-    }
-    sort_bubble2(ary2);
-    task_exit(0);
-}
-
-void sort_bubble1(int ary[])
+void sort_bubble(int ary[], int curConsumer)
 {
     int i, j, temp = 0;
     int gap = 2;
@@ -126,12 +108,12 @@ void sort_bubble1(int ary[])
         {
             if (ary[j] < ary[j - 1])
             {
-                line(0, (j - 1) * gap, ary[j - 1], (j - 1) * gap, RGB(0, 0, 0));
+                line(curConsumer, (j - 1) * gap, curConsumer + ary[j - 1], (j - 1) * gap, RGB(0, 0, 0));
                 temp = ary[j];
                 ary[j] = ary[j - 1];
                 ary[j - 1] = temp;
-                line(0, (j - 1) * gap, ary[j - 1], (j - 1) * gap, RGB(128, 0, 128));
-                line(0, j * gap, ary[j], j * gap, RGB(128, 0, 128));
+                line(curConsumer, (j - 1) * gap, curConsumer + ary[j - 1], (j - 1) * gap, RGB(128, 0, 128));
+                line(curConsumer, j * gap, curConsumer + ary[j], j * gap, RGB(128, 0, 128));
                 struct timespec sleepTime;
                 sleepTime.tv_sec = 0;          // seconds
                 sleepTime.tv_nsec = 100000000; // nanoseconds
@@ -140,87 +122,101 @@ void sort_bubble1(int ary[])
         }
     }
 }
-void sort_bubble2(int ary[])
-{
-    int i, j, temp = 0;
-    int gap = 2;
 
-    for (i = 1; i < N; i++)
+void tsk_producer(void *pv)
+{
+    int i; // 循环变量
+    int width = g_graphic_dev.XResolution / 10;
+    int gap = 2;
+    while (pv)
     {
-        for (j = i; j > 0; j--)
+        sem_wait(&empty);
+        sem_wait(&mutex);
+        for (i = 0; i < N; i++)
         {
-            if (ary[j] < ary[j - 1])
-            {
-                line(400, (j - 1) * gap, 400 + ary[j - 1], (j - 1) * gap, RGB(0, 0, 0));
-                temp = ary[j];
-                ary[j] = ary[j - 1];
-                ary[j - 1] = temp;
-                line(400, (j - 1) * gap, 400 + ary[j - 1], (j - 1) * gap, RGB(255, 165, 0));
-                line(400, j * gap, 400 + ary[j], j * gap, RGB(255, 165, 0));
-                struct timespec sleepTime;
-                sleepTime.tv_sec = 0;          // seconds
-                sleepTime.tv_nsec = 100000000; // nanoseconds
-                nanosleep(&sleepTime, NULL);
-            }
+            line(curProducer, i * gap, curProducer + ary[i], i * gap, RGB(128, 0, 128));
         }
+        curProducer = (curProducer + 1) % 10;
+        sem_signal(&mutex);
+        sem_signal(&full);
+    }
+    task_exit(0);
+}
+
+void tsk_consumer(void *pv)
+{
+    int i;
+    // 分十份 每份占十分之一
+    int width = g_graphic_dev.XResolution / 10;
+    while (pv)
+    {
+        sem_wait(&full);
+        sem_wait(mutex);
+        sort_bubble(ary, curConsumer); // 对消费者消费的数据进行排序
+        curConsumer = (curConsumer + 1) % 10;
+        sem_signal(&mutex);
+        sem_signal(&empty);
     }
 }
-void tsk_3(void *pv)
+
+void tsk_control(void *pv)
 {
     /*
     事实上这个getpriority和setpriority是获取、改变的nice值，nice值越小优先级越高。
     然而为了可视化，需要做一个运算，具体运算体现在setpriority函数中。
     */
+    // 这里面的line是在可视化nice
     int i;
+    int nice_consumer = getpriority(tid_consumer); // 获取线程优先级
+    int nice_producer = getpriority(tid_producer);
+    // 左边
 
-    int nice1 = getpriority(tid1); // 获取线程优先级
-    int nice2 = getpriority(tid2);
-
-    for (i = 0; i <= nice1; i++) // 画出nice的实体
+    for (i = 0; i <= nice_producer; i++) // 画出nice的实体
         line(g_graphic_dev.XResolution / 2 - i * emp2 - 1, g_graphic_dev.YResolution * 3 / 4 - 25,
              g_graphic_dev.XResolution / 2 - i * emp2 - 1, g_graphic_dev.YResolution * 3 / 4 + 25, RGB(128, 0, 128));
-    for (i = 0; i <= nice2; i++)
+    // 右边
+    for (i = 0; i <= nice_consumer; i++)
         line(g_graphic_dev.XResolution / 2 + i * emp2 + 1, g_graphic_dev.YResolution * 3 / 4 - 25,
              g_graphic_dev.XResolution / 2 + i * emp2 + 1, g_graphic_dev.YResolution * 3 / 4 + 25, RGB(255, 165, 0));
 
     do
     {
         int key = getchar();
-        if (key == 0x4800) // up 提高任务1的优先度
+        if (key == 0x4800) // up 提高生产者producer的优先度
         {
-            nice1 = nice1 + 1;
-            if (nice1 > PRI_USER_MAX)
-                nice1 = PRI_USER_MAX;
-            setpriority(tid1, PRI_USER_MAX - nice1);
-            line(g_graphic_dev.XResolution / 2 - nice1 * emp2 - 1, g_graphic_dev.YResolution * 3 / 4 - 25,
-                 g_graphic_dev.XResolution / 2 - nice1 * emp2 - 1, g_graphic_dev.YResolution * 3 / 4 + 25, RGB(128, 0, 128));
+            nice_producer = nice_producer + 1;
+            if (nice_producer > PRI_USER_MAX)
+                nice_producer = PRI_USER_MAX;
+            setpriority(tid_producer, PRI_USER_MAX - nice_producer);
+            line(g_graphic_dev.XResolution / 2 - nice_producer * emp2 - 1, g_graphic_dev.YResolution * 3 / 4 - 25,
+                 g_graphic_dev.XResolution / 2 - nice_producer * emp2 - 1, g_graphic_dev.YResolution * 3 / 4 + 25, RGB(128, 0, 128));
         }
-        else if (key == 0x5000) // down 降低任务1的优先度
+        else if (key == 0x5000) // down 降低生产者prouducer的优先度
         {
-            line(g_graphic_dev.XResolution / 2 - nice1 * emp2 - 1, g_graphic_dev.YResolution * 3 / 4 - 25,
-                 g_graphic_dev.XResolution / 2 - nice1 * emp2 - 1, g_graphic_dev.YResolution * 3 / 4 + 25, RGB(0, 0, 0)); // 消失涂黑
-            nice1 = nice1 - 1;
-            if (nice1 < PRI_USER_MIN)
-                nice1 = PRI_USER_MIN;
-            setpriority(tid1, PRI_USER_MAX - nice1);
+            line(g_graphic_dev.XResolution / 2 - nice_producer * emp2 - 1, g_graphic_dev.YResolution * 3 / 4 - 25,
+                 g_graphic_dev.XResolution / 2 - nice_producer * emp2 - 1, g_graphic_dev.YResolution * 3 / 4 + 25, RGB(0, 0, 0)); // 消失涂黑
+            nice_producer = nice_producer - 1;
+            if (nice_producer < PRI_USER_MIN)
+                nice_producer = PRI_USER_MIN;
+            setpriority(tid_producer, PRI_USER_MAX - nice_producer);
         }
-        if (key == 0x4D00) // right 提高任务2的优先度
+        if (key == 0x4D00) // right 提高消费者consumer的优先度
         {
-            nice2 = nice2 + 1;
-            if (nice2 > PRI_USER_MAX)
-                nice2 = PRI_USER_MAX;
-            setpriority(tid2, PRI_USER_MAX - nice2);
-            line(g_graphic_dev.XResolution / 2 + nice2 * emp2 + 1, g_graphic_dev.YResolution * 3 / 4 - 25,
-                 g_graphic_dev.XResolution / 2 + nice2 * emp2 + 1, g_graphic_dev.YResolution * 3 / 4 + 25, RGB(255, 165, 0));
+            nice_consumer = nice_consumer + 1;
+            if (nice_consumer > PRI_USER_MAX)
+                nice_consumer = PRI_USER_MAX;
+            setpriority(tid_consumer, PRI_USER_MAX - nice_consumer);
+            line(g_graphic_dev.XResolution / 2 + nice_consumer * emp2 + 1, g_graphic_dev.YResolution * 3 / 4 - 25,
+                 g_graphic_dev.XResolution / 2 + nice_consumer * emp2 + 1, g_graphic_dev.YResolution * 3 / 4 + 25, RGB(255, 165, 0));
         }
-        else if (key == 0x4B00) // left 降低任务2的优先度
+        else if (key == 0x4B00) // left 降低消费者consumer的优先度
         {
-            line(g_graphic_dev.XResolution / 2 + nice2 * emp2 + 1, g_graphic_dev.YResolution * 3 / 4 - 25,
-                 g_graphic_dev.XResolution / 2 + nice2 * emp2 + 1, g_graphic_dev.YResolution * 3 / 4 + 25, RGB(0, 0, 0));
-            nice2 = nice2 - 1;
-            if (nice2 < PRI_USER_MIN)
-                nice2 = PRI_USER_MIN;
-            setpriority(tid2, PRI_USER_MAX - nice2);
+            line(g_graphic_dev.XResolution / 2 + nice_consumer * emp2 + 1, g_graphic_dev.YResolution * 3 / 4 - 25,
+                 g_graphic_dev.XResolution / 2 + nice_consumer * emp2 + 1, g_graphic_dev.YResolution * 3 / 4 + 25, RGB(0, 0, 0));
+            nice_consumer = nice_consumer - 1;
+            if (nice_consumer < PRI_USER_MIN)
+                nice_consumer = PRI_USER_MIN;
+            setpriority(tid_consumer, PRI_USER_MAX - nice_consumer);
         }
     } while (1); // 重复获取键盘输入
 
